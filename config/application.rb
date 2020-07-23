f0327cbe5ccc48e003294939681601dfafed59f0
kazusa.saito <3110kazusa@gmail.com>
2020-07-24 06:41:49 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -14,12 +14,11 @@ module VueRailsSample
     config.load_defaults 6.0
 
     config.generators do |g|
-      g.test_framework :rspec,
-        fixtures: false,
-        view_specs: false,
-        helper_specs: false,
-        controller_specs: false,
-        routing_specs: false
+      g.test_framework :rspec, fixtures: false,
+                               view_specs: false,
+                               helper_specs: false,
+                               controller_specs: false,
+                               routing_specs: false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
